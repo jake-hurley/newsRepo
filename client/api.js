2f969e9a68aa65c1b8ae2ae6c89fcfff92fd4013
Jake-Hurley <jshurley97@gmail.com>
2020-10-21 14:33:40 +1300

    home component logs articles from api call

--- client/api.js
+++ client/api.js
@@ -1,5 +1,12 @@
 
-
-
-const APIURL = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${APIKEY}`
+const APIURL = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=`
 const APIKEY = '3055f8f90fa44bbe8bda05385a20690a'
+
+
+export function getAllNews () {
+    // console.log(APIURL + APIKEY)
+    return fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3055f8f90fa44bbe8bda05385a20690a')
+    .then(response => {
+        return response.json()
+    })
+}
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { getAllNews } from '../api'
+
 class Home extends React.Component {
+
+  state = {
+    articles: [],
+    isLoaded: false
+  }
+
+  componentDidMount () {
+    getAllNews()
+    .then(data => {
+      console.log(data.articles)
+    })
+  }
+
   render () {
   return (
     <>
