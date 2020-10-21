01e5b374ae3e790a98d7af8f2d05fd663aa40975
Jake-Hurley <jshurley97@gmail.com>
2020-10-21 14:17:03 +1300

    initial commit setting up base components and API

---
+++ client/api.js
@@ -0,0 +1,5 @@
+
+
+
+const APIURL = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${APIKEY}`
+const APIKEY = '3055f8f90fa44bbe8bda05385a20690a'
---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Home extends React.Component {
+  render () {
+  return (
+    <>
+      <h1>News</h1>
+    </>
+  )
+}
+}
+
+export default Home
